12af82f9689cf9e171d067ca958d2c62bbba4ced
Cody Seibert <cseibert113@gmail.com>
2022-12-30 17:42:20 -0500

    drawing out a basic map

--- src/createGameLoop.ts
+++ src/createGameLoop.ts
@@ -1,11 +1,18 @@
+import type { Application } from 'pixi.js';
 import { getEntityById } from './EntityManager';
 import { updatePlayer, type TPlayerEntity } from './PlayerEntity';
 
-export function createGameLoop() {
+export function createGameLoop(app: Application) {
   function tick() {
     const player = getEntityById('player')! as unknown as TPlayerEntity;
 
     updatePlayer(player);
+
+    const { x: playerX, y: playerY } = player.sprite.position;
+    const cx = playerX - app.screen.width / 2;
+    const cy = playerY - app.screen.height / 2;
+
+    app.stage.position.set(-cx, -cy);
 
     window.requestAnimationFrame(tick);
   }
