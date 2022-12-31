95f9f6f60ef5c52df54fe7e030c4d7f1cca9be1e
Cody Seibert <cseibert113@gmail.com>
2022-12-31 15:05:42 -0500

    adding interaction with stairs

---
+++ src/entity/components/InteractIntent.ts
@@ -0,0 +1,31 @@
+import type { ECSComponent } from '@/ecs/ECSComponent';
+import type { ECSEntity } from '@/ecs/ECSEntity';
+
+export const InteractIntentBrand = 'interact_intent';
+type InteractIntentBrand = typeof InteractIntentBrand;
+export type InteractIntent = ECSComponent<
+  InteractIntentBrand,
+  {
+    isInteracting: boolean;
+    canInteract: boolean;
+    cooldown: number;
+  }
+>;
+
+export function interactIntentComponent(cooldown = 1000): InteractIntent {
+  return {
+    [InteractIntentBrand]: {
+      isInteracting: false,
+      canInteract: true,
+      cooldown
+    }
+  };
+}
+
+export function hasInteractIntent<E extends ECSEntity>(
+  e: E
+): e is E & InteractIntent {
+  return InteractIntentBrand in e;
+}
+
+export const withInteractIntent = () => interactIntentComponent;
--- src/systems/InteractionSystem.ts
+++ src/systems/InteractionSystem.ts
@@ -1,6 +1,7 @@
 import type { ECSSystem } from '@/ecs/ECSSystem';
 import type { ECSWorld } from '@/ecs/ECSWorld';
 import type { Interactable } from '@/entity/components/Interactable';
+import type { InteractIntent } from '@/entity/components/InteractIntent';
 import type { Player } from '@/entity/components/Player';
 import type { Position } from '@/entity/components/Position';
 import type { Renderable } from '@/entity/components/Renderable';
@@ -18,14 +19,22 @@ export const InteractionSystem: (
   target: ['position', 'interactable', 'renderable'],
   run: entities => {
     entities.forEach(e => {
-      const player = world.entitiesByComponent<[Player, Position]>([
-        'player',
-        'position'
-      ])[0];
+      const player = world.entitiesByComponent<
+        [Player, Position, InteractIntent]
+      >(['player', 'position', 'interact_intent'])[0];
       if (!player) return;
       const distance = dist(e.position, player.position);
       const text = resolveSprite(e.renderable.sprite);
-      text.visible = distance < 80;
+      text.visible = distance < e.interactable.interactionRadius;
+
+      if (player.interact_intent.canInteract) {
+        player.interact_intent.canInteract = false;
+        player.interact_intent.isInteracting = false;
+        setTimeout(() => {
+          player.interact_intent.canInteract = true;
+        }, player.interact_intent.cooldown);
+        console.log('go down stairs');
+      }
     });
   }
 });
