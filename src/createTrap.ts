b3f07b9e95d715d465b8c0b3671d4fbf9b7b2a39
Cody Seibert <cseibert113@gmail.com>
2023-01-04 20:17:42 -0500

    fixing lint error

--- src/createTrap.ts
+++ src/createTrap.ts
@@ -15,7 +15,6 @@ import { createTrapStateMachine } from './stateMachines/trap';
 import { registerStateMachine } from './stateMachines/stateMachineManager';
 import { enemyComponent } from './entity/components/Enemy';
 import { withSize } from './entity/components/Size';
-import { poisonComponent } from './entity/components/Poison';
 
 export type CreateTrapOptions = {
   spriteName: SpriteName;
